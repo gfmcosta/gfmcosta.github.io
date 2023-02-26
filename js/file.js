e032afe05ff9bea198c64f8a6dfa39855eb81ee8
Goncalo Costa <gfmcosta03@gmail.com>
2023-02-26 03:27:47 +0000

    made all in one page. nav bar removed. about me removed

--- js/file.js
+++ js/file.js
@@ -1,4 +1,46 @@
+function $(id) {
+    return document.getElementById(id);
+  }
+
 function inviteToDiscord(){
     window.open('https://discord.gg/hp2jUrGADp');
 }
+function inic(){
+    create_table();
+}
 
+function create_table(){
+    let request = new XMLHttpRequest();
+    request.open('GET','https://api.github.com/users/gfmcosta/repos',true);
+
+    request.onload = function(){
+        let data = JSON.parse(this.response);
+        let codHTML=''
+        let ordem = 0
+        let rotate=false
+        $.each(data,function(i,status){
+            ordem=ordem+1
+            if(rotate){
+                codHTML+='<tr style="background-color:#FFFFFF;color:black">'
+                codHTML+='<th scope="row">'+ordem+'</th>'
+                codHTML+='<td>'+status.name+'</td>'
+                codHTML+='<td>'+status.html_url+'</td>'
+                codHTML+='<td>'+status.language+'</td>'
+                codHTML+='</tr>'
+                rotate=false
+            }else{
+                codHTML+='<tr style="background-color:#E8E8E8;color:black">'
+                codHTML+='<th scope="row">'+ordem+'</th>'
+                codHTML+='<td>'+status.name+'</td>'
+                codHTML+='<td>'+status.html_url+'</td>'
+                codHTML+='<td>'+status.language+'</td>'
+                codHTML+='</tr>'
+                rotate=true
+            }
+            
+        });
+
+        $('tbody').html(codHTML);
+    }
+    request.send();
+}
